6b659330b4331c667e50d858d5daf9e54d29fbc6
remi <remi.paulin@prestashop.com>
2022-11-05 20:45:41 +0100

    clean up

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import functions_framework
 import functools
 import requests
-import logging
 import os
 
-LOG_LEVEL = os.environ.get('LOG_LEVEL', 'WARNING').upper()
-logging.basicConfig(level=LOG_LEVEL)
-
 TECHS = {
-    'prestashop': 'PrestaShop'
+    'prestashop': 'PrestaShop',
 }
 
 
@@ -17,51 +13,36 @@ class BuiltWith:
         self._base_url = 'https://api.builtwith.com'
         self._api_key = api_key
 
-    def _get(self, path, offset, params={}):
-        params = {'KEY': self._api_key} | params
-        # 'META': 'yes'
-        # 'ALL': 'true'
-        params = "&".join("%s=%s" % (k, v) for k, v in params.items())
+    def list(self, tech, offset):
+        params = {'KEY': self._api_key, 'TECH': tech, 'META': 'yes'}
+        params = "&".join("%s=%s" % (key, value)
+                          for key, value in params.items())
         if offset is not None:
             params += f'&OFFSET={offset}'
 
-        path = os.path.join(self._base_url, path) + '?' + params
-        logging.warning(f'URL: {path}')
+        path = os.path.join(self._base_url, 'lists11/api.json') + '?' + params
+        print(f'URL: {path}')
 
         response = requests.get(path)
         response.raise_for_status()
         response_json = response.json()
 
-        results = response_json.get('Results')
+        result = response_json.get('Results')
         offset = response_json.get('NextOffset')
         has_more = (offset != 'END')
         offset = offset if has_more else None
 
-        logging.warning(f'offset: {offset}')
-        logging.warning(f'has_more: {has_more}')
-
-        return results, offset, has_more
-
-    def list(self, tech, offset):
-        path = 'lists11/api.json'
-        params = {'TECH': tech}
-        # since = '1%20Days%20Ago'
-        # params = params | {'SINCE': since}
-        results, offset, has_more = self._get(path, offset, params)
-        return results, offset, has_more
-
-    def listN(self, tech, offset, n):
-        pass
+        return result, offset, has_more
 
 
 @functions_framework.http
 def builtwith(request):
     request_json = request.get_json()
-    logging.warning(f'request: {request_json}')
+    print(f'request_json: {request_json}')
 
     state = request_json.get('state')
     if state is None:
-        return 'Error: missing state value', 400
+        return 'Error: missing state', 400
 
     secrets = request_json.get('secrets', {})
     api_key = secrets.get('api_key')
@@ -70,7 +51,7 @@ def builtwith(request):
 
     builtwith = BuiltWith(api_key)
 
-    schema = {k: {'primary_key': ['D']} for (k, v) in TECHS.items()}
+    schema = {key: {'primary_key': ['D']} for key in TECHS.keys()}
 
     if request_json.get('setup_test'):
         return {
@@ -79,13 +60,13 @@ def builtwith(request):
             'hasMore': False
         }, 200
 
-    results = {k: builtwith.list(v, state.get(k)) for (k, v) in TECHS.items()}
-
-    insert = {k: v[0] for (k, v) in results.items()}
-    state = {k: v[1] for (k, v) in results.items()}
+    results = {key: builtwith.list(tech, state.get(key))
+               for (key, tech) in TECHS.items()}
+    insert = {key: result[0] for (key, result) in results.items()}
+    state = {key: result[1] for (key, result) in results.items()}
 
     has_more = functools.reduce(
-        lambda a, b: a or b, [v[2] for v in results.values()], False)
+        lambda a, b: a or b, [result[2] for result in results.values()], False)
 
     return {
         'state': state,
